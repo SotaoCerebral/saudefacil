9881559da27e302b108acfeb2c707a1cd54a1bc0
SotaoCerebral <matheusteixeira358@gmail.com>
2025-05-17 17:24:56 -0300

    deploy render

---
+++ demo-Saude-Facil/src/test/java/com/saude/facil/demo/service/AgendamentoServiceTest.java
@@ -0,0 +1,24 @@
+package com.saude.facil.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AgendamentoServiceTest {
+
+    @Test
+    void criarAgendamento() {
+    }
+
+    @Test
+    void buscarAgendamentoPorId() {
+    }
+
+    @Test
+    void cancelarAgendamento() {
+    }
+
+    @Test
+    void listarAgendamentos() {
+    }
+}
